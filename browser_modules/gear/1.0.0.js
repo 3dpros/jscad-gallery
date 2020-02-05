80ced8b730f4ba81c5748318095cc6cd8d06ab8b
Al <Al@SELWYN>
2020-02-05 15:28:33 -0600

    testing

--- browser_modules/gear/1.0.0.js
+++ browser_modules/gear/1.0.0.js
@@ -11,129 +11,57 @@ const { circle, polygon, square, triangle } = scadApi.primitives2d;
 const { cube, cylinder, geodesicSphere, polyhedron, sphere, torus } = scadApi.primitives3d;
 const { center, chain_hull, contract, expand, hull, minkowski, mirror, multmatrix } = scadApi.transformations;
 
-// title      : Gear
-// author     : Joost Nieuwenhuijse
-// license    : MIT License
-// description: a simple gear
-// file       : gear.jscad
+///////////////
 
-// Here we define the user editable parameters: 
-function getParameterDefinitions() {
+// title      : Name Plate
+// author     : Rene K. Mueller
+// license    : MIT License
+// description: create your own name plate
+// date       : 2013/04/24
+// file       : name_plate.jscad
+
+function getParameterDefinitions () {
   return [
-    { name: 'numTeeth', caption: 'Number of teeth:', type: 'int', initial: 10, min: 5,max: 20, step: 1 },
-    { name: 'circularPitch', caption: 'Circular pitch:', type: 'float', initial: 5 },
-    { name: 'pressureAngle', caption: 'Pressure angle:', type: 'float', initial: 20 },
-    { name: 'clearance', caption: 'Clearance:', type: 'float', initial: 0 },
-    { name: 'thickness', caption: 'Thickness:', type: 'float', initial: 5 },
-    { name: 'centerholeradius', caption: 'Radius of center hole (0 for no hole):', type: 'float', initial: 2 }
+    {name: 'name', initial: 'Joe Example', type: 'text', caption: 'Your name', size: 30},
+    {name: 'title', initial: '3D Printer Developer', type: 'text', caption: 'Your title', size: 30},
+    {name: 'thickness', initial: 3, type: 'float', caption: 'Thickness'}
   ];
 }
 
-// Main entry point; here we construct our solid: 
-function main(params)
-{
-  var gear = involuteGear(
-    params.numTeeth,
-    params.circularPitch,
-    params.pressureAngle,
-    params.clearance,
-    params.thickness
-  );
-  if(params.centerholeradius > 0)
-  {
-    var centerhole = CSG.cylinder({start: [0,0,-params.thickness], end: [0,0,params.thickness], radius: params.centerholeradius, resolution: 16});
-    gear = gear.subtract(centerhole);
+function main (param) {
+  var o = []; // our stack of objects
+  var l = []; // our stack of line segments (when rendering vector text)
+  var p = []; // our stack of extruded line segments
+
+  // -- render name & extrude
+  l = vector_text(0, 0, param.name);
+  l.forEach(function (s) {
+    p.push(rectangular_extrude(s, {w: param.thickness, h: param.thickness}));
+  });
+  o.push(union(p).setColor([1, 1, 0]).scale([1 / 3, 1 / 3, 1 / 3]).center([true, true, false]).translate([0, 0, param.thickness]));
+
+  if (param.title.length) {
+    // -- render title & extrude
+    l = vector_text(0, 0, param.title);
+    p = [];
+    l.forEach(function (s) {
+      p.push(rectangular_extrude(s, {w: param.thickness, h: param.thickness}));
+    });
+    o.push(union(p).setColor([1, 1, 0]).scale([1 / 8, 1 / 8, 1 / 3]).center([true, true, false]).translate([0, -8, param.thickness]));
   }
-  return gear;
-}
+  o = [union(o)]; // neat: we combine name + title, and make it first entry of an array
 
-/*
-  For gear terminology see: 
-    http://www.astronomiainumbria.org/advanced_internet_files/meccanica/easyweb.easynet.co.uk/_chrish/geardata.htm
-  Algorithm based on:
-    http://www.cartertools.com/involute.html
+  var b = o[0].getBounds();
+  var m = 2;
+  var w = b[1].x - b[0].x + m * 2;
+  var h = b[1].y - b[0].y + m * 2;
+  o.push(cube({size: [w, h, param.thickness], round: true, radius: 0.5}).translate([b[0].x - m, b[0].y - m, 0]));
 
-  circularPitch: The distance between adjacent teeth measured at the pitch circle
-*/ 
-function involuteGear(numTeeth, circularPitch, pressureAngle, clearance, thickness)
-{
-  // default values:
-  if(arguments.length < 3) pressureAngle = 20;
-  if(arguments.length < 4) clearance = 0;
-  if(arguments.length < 4) thickness = 1;
-  
-  var addendum = circularPitch / Math.PI;
-  var dedendum = addendum + clearance;
-  
-  // radiuses of the 4 circles:
-  var pitchRadius = numTeeth * circularPitch / (2 * Math.PI);
-  var baseRadius = pitchRadius * Math.cos(Math.PI * pressureAngle / 180);
-  var outerRadius = pitchRadius + addendum;
-  var rootRadius = pitchRadius - dedendum;
-
-  var maxtanlength = Math.sqrt(outerRadius*outerRadius - baseRadius*baseRadius);
-  var maxangle = maxtanlength / baseRadius;
-
-  var tl_at_pitchcircle = Math.sqrt(pitchRadius*pitchRadius - baseRadius*baseRadius);
-  var angle_at_pitchcircle = tl_at_pitchcircle / baseRadius;
-  var diffangle = angle_at_pitchcircle - Math.atan(angle_at_pitchcircle);
-  var angularToothWidthAtBase = Math.PI / numTeeth + 2*diffangle;
-
-  // build a single 2d tooth in the 'points' array:
-  var resolution = 5;
-  var points = [new CSG.Vector2D(0,0)];
-  for(var i = 0; i <= resolution; i++)
-  {
-    // first side of the tooth:
-    var angle = maxangle * i / resolution;
-    var tanlength = angle * baseRadius;
-    var radvector = CSG.Vector2D.fromAngle(angle);    
-    var tanvector = radvector.normal();
-    var p = radvector.times(baseRadius).plus(tanvector.times(tanlength));
-    points[i+1] = p;
-    
-    // opposite side of the tooth:
-    radvector = CSG.Vector2D.fromAngle(angularToothWidthAtBase - angle);    
-    tanvector = radvector.normal().negated();
-    p = radvector.times(baseRadius).plus(tanvector.times(tanlength));
-    points[2 * resolution + 2 - i] = p;
-  }
-
-  // create the polygon and extrude into 3D:
-  var tooth3d = new CSG.Polygon2D(points).extrude({offset: [0, 0, thickness]});
-
-  var allteeth = new CSG();
-  for(var j = 0; j < numTeeth; j++)
-  {
-    var ang = j*360/numTeeth;
-    var rotatedtooth = tooth3d.rotateZ(ang);
-    allteeth = allteeth.unionForNonIntersecting(rotatedtooth);
-  }
-
-  // build the root circle:  
-  points = [];
-  var toothAngle = 2 * Math.PI / numTeeth;
-  var toothCenterAngle = 0.5 * angularToothWidthAtBase; 
-  for(var k = 0; k < numTeeth; k++)
-  {
-    var angl = toothCenterAngle + k * toothAngle;
-    var p1 = CSG.Vector2D.fromAngle(angl).times(rootRadius);
-    points.push(p1);
-  }
-
-  // create the polygon and extrude into 3D:
-  var rootcircle = new CSG.Polygon2D(points).extrude({offset: [0, 0, thickness]});
-
-  var result = rootcircle.union(allteeth);
-
-  // center at origin:
-  result = result.translate([0, 0, -thickness/2]);
-
-  return result;
+  return union(o);
 }
 
 
-
+///////////////
 module.exports = main;
 
 if (typeof getParameterDefinitions === 'function') {
